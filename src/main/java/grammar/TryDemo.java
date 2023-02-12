0b74bff9ff25063cbe0b5a2714ba681cb8d4dd85
Big-Totoro@HUAWEI <623966322@163.com>
2023-02-12 18:02:35 +0800

    知识点

---
+++ src/main/java/grammar/TryDemo.java
@@ -0,0 +1,18 @@
+package grammar;
+
+
+public class TryDemo {
+    public static void main(String[] args) {
+        System.out.println(test1());
+    }
+
+    public static int test1() {
+        int i = 0;
+        try {
+            i = 2;
+            return i;
+        } finally {
+            i = 3;
+        }
+    }
+}
