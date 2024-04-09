95b1b2f92b24d727af1293c76beb24d0cc443485
Big-Totoro@HUAWEI <623966322@163.com>
2024-04-09 22:48:00 +0800

    反转链表between

--- src/main/java/kexin/letcode/链表/ReverseListNode.java
+++ src/main/java/kexin/letcode/链表/ReverseListNode.java
@@ -4,22 +4,37 @@ public class ReverseListNode {
 
     public static void main(String[] args) {
         ListNode listNode = ListNode.buildListNode(new int[]{1, 2, 3, 4, 5});
-        ListNode reversed = reverseBetween(listNode, 2, 4);
-        System.out.println(reversed);
+        ListNode reversed1 = reverse(listNode);
+        // ListNode reversed = reverseBetween(listNode, 2, 4);
+        System.out.println(reversed1);
     }
 
     public static ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode cur = head;
-        ListNode tail = null;
+        ListNode pre = new ListNode(-1);
+        ListNode mid = null;
+        ListNode tail = new ListNode(-2);
         int count = 1;
-        while (cur != null) {
-            if (count >= left && count <= right) {
-                cur.next = tail;
-                tail = cur;
+        while (head != null) {
+            if (count < left) {
+
+            } else if (count >= left && count <= right) {
+
+            } else {
+
             }
-            cur = cur.next;
             count++;
         }
         return tail;
+    }
+
+    public static ListNode reverse(ListNode listNode) {
+        ListNode pre = null;
+        while (listNode != null) {
+            ListNode temp = listNode.next;
+            listNode.next = pre;
+            pre = listNode;
+            listNode = temp;
+        }
+        return pre;
     }
 }
