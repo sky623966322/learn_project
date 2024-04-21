f62bdaf5bd25033907ad4af22d4ad1df8cd1a9e7
Big-Totoro@HUAWEI <623966322@163.com>
2024-04-21 13:58:10 +0800

    二叉树，层序遍历

--- src/main/java/kexin/letcode/二叉树/TreeNode.java
+++ src/main/java/kexin/letcode/二叉树/TreeNode.java
@@ -1,7 +1,9 @@
 package kexin.letcode.二叉树;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 public class TreeNode {
     int val;
@@ -27,17 +29,32 @@ public class TreeNode {
         TreeNode treeNode2 = new TreeNode(2, treeNode3, treeNode4);
         TreeNode treeNode5 = new TreeNode(5);
         TreeNode treeNode1 = new TreeNode(1, treeNode2, treeNode5);
-        List<Integer> list = inorderTraversal(treeNode1);
+       /* List<Integer> list = inorderTraversal(treeNode1);
         StringBuilder sb = new StringBuilder();
         list.forEach(item-> sb.append(item).append(" "));
-        System.out.println(sb);
+        System.out.println(sb);*/
+        List<List<Integer>> levelOrder = levelOrder(treeNode1);
+        System.out.println(levelOrder);
     }
 
     public static List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        // inOrder(root, list);
-        // preOrder(root, list);
-        postOrder(root, list);
+        inOrder(root, list);
+        return list;
+    }
+
+    public static void inOrder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left, list);
+        list.add(root.val);
+        inOrder(root.right, list);
+    }
+
+    public static List<Integer> preTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        preOrder(root, list);
         return list;
     }
 
@@ -50,13 +67,10 @@ public class TreeNode {
         preOrder(root.right, list);
     }
 
-    public static void inOrder(TreeNode root, List<Integer> list) {
-        if (root == null) {
-            return;
-        }
-        inOrder(root.left, list);
-        list.add(root.val);
-        inOrder(root.right, list);
+    public static List<Integer> postTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        preOrder(root, list);
+        return list;
     }
 
     public static void postOrder(TreeNode root, List<Integer> list) {
@@ -66,6 +80,23 @@ public class TreeNode {
         postOrder(root.left, list);
         postOrder(root.right, list);
         list.add(root.val);
+    }
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (queue.size() > 0) {
+            List<Integer> tmp = new ArrayList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode item = queue.poll();
+                tmp.add(item.val);
+                if (item.left != null) queue.add(item.left);
+                if (item.right != null) queue.add(item.right);
+            }
+            result.add(tmp);
+        }
+        return result;
     }
 
 }
