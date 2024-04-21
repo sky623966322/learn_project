d0f94fcfc7a643e23ce1d49c17df0d2e48d504dc
Big-Totoro@HUAWEI <623966322@163.com>
2024-04-21 13:57:41 +0800

    二叉树。相同的树

---
+++ src/main/java/kexin/letcode/二叉树/IsSameTree.java
@@ -0,0 +1,15 @@
+package kexin.letcode.二叉树;
+
+public class IsSameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else {
+            return false;
+        }
+    }
+
+}
