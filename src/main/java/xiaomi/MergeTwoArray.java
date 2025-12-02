b1c76a4b3b76e2feb11079afcafd90413a25d50a
Big-Totoro@HUAWEI <623966322@163.com>
2025-12-02 22:52:08 +0800

    两个无序整型数组合并成一个有序数组，并去重

--- src/main/java/xiaomi/MergeTwoArray.java
+++ src/main/java/xiaomi/MergeTwoArray.java
@@ -2,61 +2,74 @@ package xiaomi;
 
 /**
  * 两个无序整型数组合并成一个有序数组，并去重，
- *
+ * <p>
  * 要求：1、硬编码 2、不能使用集合和集合方法
  */
 public class MergeTwoArray {
     public static void main(String[] args) {
-        int[] nums1 = new int[]{2, 5, 3}; // 2， 3， 5
-        int[] nums2 = new int[]{9, 2, 8}; // 2, 8, 9
-
+        int[] num1 = new int[]{9,7,3};
+        int[] num2 = new int[]{8,5,3};
+        int[] sortUnique = mergeAndSortUnique(num1, num2);
+        System.out.println(sortUnique);
     }
 
-   public static int[] mergeAndSortUnique(int[] nums1, int[] nums2){
-       /**
-        * 1、先将nums1、nums2合并
-        * 2、对合并后的数组去重
-        * 3、对去重后的数组排序
-        */
-       // 合并
-       int length1 = nums1.length;
-       int length2 = nums2.length;
-       int[] mergeArray = new int[]{length1 + length2};
-       for (int i = 0; i < length1; i++) {
-           mergeArray[i] = nums1[i];
-       }
-       for (int i = 0; i < length2; i++) {
-           mergeArray[length1 + i] = nums2[i];
-       }
-       // 排序
-
-        return mergeArray;
-   }
-
-    public static int[] merge(int[] nums1, int[] nums2) {
-        int[] sort1 = sortAndUnique(nums1);
-        int[] sort2 = sortAndUnique(nums2);
-
-        int p1 = 0;
-        int p2 = 0;
-        int count = 0;
-        int[] result = new int[sort1.length + sort2.length];
-        while (p1 < sort1.length || p1 < sort2.length) {
-            if (sort1[p1] <= sort2[p2]) {
-                result[count] = sort1[p1];
-                p1++;
-            } else if (sort1[p1] > sort2[p2]) {
-                result[count] = sort2[p2];
-                p2++;
-            }
-            count ++;
+    public static int[] mergeAndSortUnique(int[] nums1, int[] nums2) {
+        /**
+         * 1、先将nums1、nums2合并
+         * 2、对合并后的数组去重
+         * 3、对去重后的数组排序
+         */
+        // 合并
+        int length1 = nums1.length;
+        int length2 = nums2.length;
+        int[] mergeArray = new int[length1 + length2];
+        for (int i = 0; i < length1; i++) {
+            mergeArray[i] = nums1[i];
         }
-        return result;
-    }
-
-    public static int[] sortAndUnique(int[] nums) {
-        int[] result = new int[nums.length];
-
+        for (int i = 0; i < length2; i++) {
+            mergeArray[length1 + i] = nums2[i];
+        }
+        // 去重
+        int length = mergeArray.length;
+        int[] tempArray = new int[length];
+        int uniqueCount = 0;
+        for (int i = 0; i < length; i++) {
+            boolean isDuplicate = false;
+            for (int j = i + 1; j < length; j++) {
+                if (mergeArray[i] == mergeArray[j]) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            // 如果i不是重复的,则添加到tempArray
+            if(!isDuplicate) {
+                tempArray[uniqueCount] = mergeArray[i];
+                uniqueCount++;
+            }
+        }
+        // 根据tempArray创建去重后的数组
+        int[] uniqueArray = new int[uniqueCount];
+        for (int i = 0; i < uniqueCount; i++) {
+            uniqueArray[i] = tempArray[i];
+        }
+        // 对uniqueArray排序
+        /**
+         * [9,7,3,5,8]
+         *
+         * [7,3,5,8,9]
+         */
+        for (int i = 0; i < uniqueArray.length; i++) {
+            // 最后一个数字不需要往后比较，所有-1
+            for (int j = 0; j < uniqueArray.length - i - 1; j++) {
+                // 如果前面数大于后面数，则交换位置
+                if (uniqueArray[j] > uniqueArray[j + 1]) {
+                   int temp = uniqueArray[j];
+                   uniqueArray[j] = uniqueArray[j + 1];
+                   uniqueArray[j + 1] = temp;
+                }
+            }
+        }
+        return uniqueArray;
     }
 
 }
